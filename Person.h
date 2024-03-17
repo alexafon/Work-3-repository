b68c116b6a98c810fa4c0ecbfc6e59b4949eca6a
alexafon <158675700+alexafon@users.noreply.github.com>
2024-03-17 18:19:13 -0400

    Update Person.h

--- Person.h
+++ Person.h
@@ -1,1 +1,25 @@
 #pragma once
+#ifndef PERSON_H
+#define PERSON_H
+#include <string>
+using namespace std;
+
+class Person
+{
+public:
+	Person();
+	Person(string first, string last, int year, int day, int month);
+	string getName();
+	string getBirthdate();
+
+private:
+	string Fname;
+	string Lname;
+
+protected:
+	int birthDay;
+	int birthMonth;
+	int birthYear;
+};
+
+#endif
