dce530520348add404d1956c64abbf946ae579a3
alexafon <158675700+alexafon@users.noreply.github.com>
2024-03-17 18:21:42 -0400

    Update alexafon_HW3_Q1.cpp

--- alexafon_HW3_Q1.cpp
+++ alexafon_HW3_Q1.cpp
@@ -1,12 +1,26 @@
 // alexafon_HW3_Q1.cpp
 #include <iostream>
 #include "Person.h"
+#include "Employee.h"
+#include "Student.h"
+#include "GradStudent.h"
 using namespace std;
 
 int main()
 {
 	Person p1("Tony", "Stark", 1, 21, 1950);
 	cout << p1.getName() << " born on: " << p1.getBirthdate() << endl;
+
+	Employee e1("Steve", "Rogers", 7, 4, 1920, 741920);
+	cout << e1.getName() << " born on: " << e1.getBirthdate() << " has employee ID: " << e1.getEmployeeID() << endl;
+
+	Student s1("Barry", "Allen", 9, 30, 1992, 90);
+	cout << s1.getName() << " born on: " << s1.getBirthdate() << " has " << s1.getNumCredits() << " credits" << endl;
+
+	GradStudent gs1("Stephen", "Strange", 11, 18, 1930, 30);
+	cout << gs1.getName() << " born on: " << gs1.getBirthdate() << " has " << gs1.getNumCredits() << " credits and can graduate: ";
+	gs1.checkGraduation();
+	cout << gs1.canGraduate() << endl;
 
 	return 0;
 }
